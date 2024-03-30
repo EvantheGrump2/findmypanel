5c27363d578ba6fb8a7c6174d8183e31ba791dc6
EvantheGrump2 <165404156+EvantheGrump2@users.noreply.github.com>
2024-03-30 18:11:10 -0400

    Update findmypanel.sh to remove ansi coloring and add updating message

--- findmypanel.sh
+++ findmypanel.sh
@@ -1,14 +1,15 @@
-wget https://github.com/EvantheGrump2/findmypanel/blob/main/iplist.txt
+echo Updating...
+wget -q -O iplist.txt https://github.com/EvantheGrump2/findmypanel/releases/latest/download/iplist.txt
 rm output.html 2> /dev/null
 echo '<html><body>' > output.html
 for tests in google.com yahoo.com duckduckgo.com
   do
 	if curl -m 1 -Is $tests >/dev/null
 	 then
-		echo '\e[1;37msanity ping ('$tests') - \e[1;32mconnected'
+		echo 'sanity ping ('$tests') - connected'
 	 else
-		echo '\e[1;33msanity ping ('$tests') - \e[1;31mfailure!'
-		echo '\e[1;33mAre you connected to the internet?\033[0m'
+		echo 'sanity ping ('$tests') - failure!'
+		echo 'Are you connected to the internet?'
 		read -r -p "Continue anyway? {y/N} " response
 		case "$response" in
 		[yY][eE][sS]|[yY])
@@ -21,25 +22,25 @@ for tests in google.com yahoo.com duckduckgo.com
 		esac
 	fi
   done
-echo '\e[1;37mcheck complete, starting scanning'
+echo 'check complete, starting scanning'
 for ips in $(cat iplist.txt)
   do
 	if curl -m 1 -Is $ips >/dev/null
 	 then
-		echo -n '\e[1;37m'$ips' -  \e[1;32msuccess     \r'
+		echo -n ''$ips' - success     \r'
  		echo '<a href="https://'$ips'" target="_blank">'$ips'</a>' >> output.html
 		echo '<a href="http://'$ips'" target="_blank">(http)</a><br>' >> output.html
 	 else
- 		echo -n '\e[1;37m'$ips' - \e[1;31mfail       \r'
+ 		echo -n ''$ips' - fail       \r'
 	fi
   done
-echo '\e[1;37mDone \e[1;32m✓                         '
-echo '\e[1;37mopening report'
+echo 'Done ✓                         '
+echo 'opening report'
 if grep -q "1" "output.html"
   then
 	echo hi > /dev/null
   else
         echo 'no hits ):' >> output.html
 fi
-x-www-browser output.html > /dev/null
+xdg-open output.html > /dev/null
 exit
